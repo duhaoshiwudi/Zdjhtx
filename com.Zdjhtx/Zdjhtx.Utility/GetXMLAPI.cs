e5a6e3bbcb8cdf2eb4e95b13ffed54d57d4b46ab
duhaoshiwudi <duhaoshiwudi@163.com>
2016-06-03 18:35:47 +0800

    123456

---
+++ com.Zdjhtx/Zdjhtx.Utility/GetXMLAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zdjhtx.UI.Models;
+
+namespace Zdjhtx.Utility
+{
+    public static partial class GetXMLAPI
+    {
+        public static PhoneNumInfo GetPhoneNumInfo()
+        {
+            return null;
+        }
+    }
+}
