767aa9d60bd61eb81c7f768391659f802e8de5a3
duhaoshiwudi <duhaoshiwudi@163.com>
2016-06-03 23:56:05 +0800

    45

--- com.Zdjhtx/Zdjhtx.UI/Global.asax.cs
+++ com.Zdjhtx/Zdjhtx.UI/Global.asax.cs
@@ -8,7 +8,7 @@ using System.Web.Routing;
 
 namespace com.Zdjhtx
 {
-    // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
+    // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明1，
     // 请访问 http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
