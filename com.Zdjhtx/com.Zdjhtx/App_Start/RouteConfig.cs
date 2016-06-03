68236a2d4eaa4a81a7f19e7e418bb5cf7621a162
duhaoshiwudi <duhaoshiwudi@163.com>
2016-06-03 14:59:46 +0800

    移动充值

--- com.Zdjhtx/com.Zdjhtx/App_Start/RouteConfig.cs
+++ com.Zdjhtx/com.Zdjhtx/App_Start/RouteConfig.cs
@@ -13,10 +13,14 @@ namespace com.Zdjhtx
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute(name: "html", url: "{controller}/{action}.html");
+            routes.MapRoute(name: "do", url: "{controller}/{action}.do");
+            routes.MapRoute(name: "api", url: "{controller}/{action}.api");
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "IndexPage", id = UrlParameter.Optional }
             );
         }
     }
